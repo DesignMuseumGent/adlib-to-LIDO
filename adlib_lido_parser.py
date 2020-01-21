7286fe77efbdd959c5f1fd28cff6bbc99743b0a5
vandhuynslagero <oliviervandhuynslager@gmail.com>
2020-01-21 22:04:37 +0100

    minor fixes

--- adlib_lido_parser.py
+++ adlib_lido_parser.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 '''ETL parse XSLT and parse XML(Adlib)  and transform XML(adlib) into XML LIDO'''
 
-dom = ET.parse("/Users/oliviervandhuynslager/PycharmProjects/DesignMuseumGent/Datasets/exports/20200120_DMG_OBJ_OBJECTSTORIES.xml")
-xslt = ET.parse("/Users/oliviervandhuynslager/PycharmProjects/DesignMuseumGent/Tools/adlib - lido/LIDO_DMG.xsl")
+dom = ET.parse("/Users/oliviervandhuynslager/Documents/GitHub/adlib-to-LIDO/adlib.xml")
+xslt = ET.parse("/Users/oliviervandhuynslager/Documents/GitHub/adlib-to-LIDO/LIDO_DMG.xsl")
 transform = ET.XSLT(xslt)
 newdom = transform(dom)
 
