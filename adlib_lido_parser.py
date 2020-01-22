d3679acfaa3b1b44c208c1aff94b30555eda47f9
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2020-01-22 22:54:32 +0100

    Update adlib_lido_parser.py

--- adlib_lido_parser.py
+++ adlib_lido_parser.py
@@ -1,19 +1,3 @@
-# #%%
-# import lxml.etree as ET
-# from xml.dom import minidom
-#
-# '''ETL parse XSLT and parse XML(Adlib)  and transform XML(adlib) into XML LIDO'''
-#
-# dom = ET.parse("/Users/oliviervandhuynslager/Documents/GitHub/adlib-to-LIDO/WOLFERS_TEST_INPUT(ADLIB).xml")
-# xslt = ET.parse("/Users/oliviervandhuynslager/Documents/GitHub/adlib-to-LIDO/LIDO_DMG.xsl")
-# transform = ET.XSLT(xslt)
-# newdom = transform(dom)
-# newdom.write("WOLFERS_TEST_OUTPUT(LIDO).xml")
-#
-# result = ET.tostring(newdom, pretty_print=True)
-# print(result)
-#
-
 #%%
 import lxml.etree as ET
 
