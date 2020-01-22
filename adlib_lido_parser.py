641f446ad3ea0ea23e1f2f0b7e267e4b619ba6b4
vandhuynslagero <oliviervandhuynslager@gmail.com>
2020-01-22 11:55:07 +0100

    test_output added

--- adlib_lido_parser.py
+++ adlib_lido_parser.py
@@ -8,6 +8,7 @@ dom = ET.parse("/Users/oliviervandhuynslager/Documents/GitHub/adlib-to-LIDO/WOLF
 xslt = ET.parse("/Users/oliviervandhuynslager/Documents/GitHub/adlib-to-LIDO/LIDO_DMG.xsl")
 transform = ET.XSLT(xslt)
 newdom = transform(dom)
+newdom.write("WOLFERS_TEST_OUTPUT(LIDO).xml")
 
 result = ET.tostring(newdom, pretty_print=True)
 print(result)
